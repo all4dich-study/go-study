7d3dfd923abef40fc67c0ac48e0cd3b23a146a8c
Sunjoo Park <all4dich@gmail.com>
2025-05-25 10:29:50 +0900

    feat(main): add printHello function
    Added a new `printHello` function that prints "Hello" to the console. This enhances the functionality of the program by providing a reusable method for printing a greeting.

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
+// TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+func printHello() {
+	fmt.Println("Hello")
+}
 
 func main() {
 	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
